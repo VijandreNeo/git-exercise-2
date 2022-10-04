784aea14d00b766b9ebf776bf4ddd5f8d95e4438
Neo Jan Erich Vijandre <neojanerich.vijandre@neojanerich.vijandre-CH4K3HF5WM>
2022-10-04 23:20:20 +0800

    added lib.js

---
+++ lib.js
@@ -0,0 +1,23 @@
+const square = (n) => {
+    return Math.sqrt(n);
+}
+
+const fahrenheit = (celsius) => {
+    return (celsius * 1.8) + 32;
+}
+
+const rectangleArea = (a, b) => {
+    return (a * b) / 2;
+}
+
+const isPalindrome = (str) => {
+    let revArr = [];
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        revArr.push(str.at(i));
+    }
+
+    let revStr = revArr.toString().replaceAll(",", "");
+
+    return revStr.toUpperCase() === str.toUpperCase();
+}
