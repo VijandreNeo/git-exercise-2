27f4c7846f55a5cf813b4d0fa224c225c2e45724
Neo Jan Erich A. Vijandre <113878032+VijandreNeo@users.noreply.github.com>
2022-10-05 09:48:19 +0800

    added lib.js (#1)
    added lib.js

---
+++ lib.js
@@ -0,0 +1,17 @@
+const square = (n) =>  n * n;
+
+const fahrenheit = (celsius) => (celsius * (9 / 5)) + 32;
+
+const rectangleArea = (a, b) =>  a * b;
+
+const isPalindrome = (str) => {
+    const revArr = [];
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        revArr.push(str.at(i));
+    }
+
+    let revStr = revArr.toString().replaceAll(",", "");
+
+    return revStr.toUpperCase() === str.toUpperCase();
+}
