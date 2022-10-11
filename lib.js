c170ce9fb92154872b09fb455e51e4a3758a4ece
Neo Jan Erich Vijandre <neojanerich.vijandre@neojanerich.vijandre-CH4K3HF5WM>
2022-10-11 09:13:36 +0800

    prettier implementation

--- lib.js
+++ lib.js
@@ -1,24 +1,24 @@
-const square = (n) =>  n * n;
+const square = (n) => n * n;
 
-const fahrenheit = (celsius) => (celsius * (9 / 5)) + 32;
+const fahrenheit = (celsius) => celsius * (9 / 5) + 32;
 
-const rectangleArea = (a, b) =>  a * b;
+const rectangleArea = (a, b) => a * b;
 
 const isPalindrome = (str) => {
-    const revArr = [];
+  const revArr = [];
 
-    for (let i = str.length - 1; i >= 0; i--) {
-        revArr.push(str.at(i));
-    }
+  for (let i = str.length - 1; i >= 0; i--) {
+    revArr.push(str.at(i));
+  }
 
-    let revStr = revArr.toString().replaceAll(",", "");
+  let revStr = revArr.toString().replaceAll(',', '');
 
-    return revStr.toUpperCase() === str.toUpperCase();
-}
+  return revStr.toUpperCase() === str.toUpperCase();
+};
 
 module.exports = {
-    square,
-    fahrenheit,
-    rectangleArea,
-    isPalindrome
-}
+  square,
+  fahrenheit,
+  rectangleArea,
+  isPalindrome,
+};
